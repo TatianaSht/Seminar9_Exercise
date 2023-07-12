50b8e42dfc85aa6d84ce2da545c41867ef274e8f
tatiana_sht <tatianaorlova0271@gmail.com>
2023-07-12 17:08:28 +1000

    Added solution

---
+++ Exercise001_SumNumber/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов 
+// в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+void Main()
+{
+    int numFirst = ReadInt ("Введите первое число: ");
+    int numSecond = ReadInt ("Введите второе число: ");
+    SumNumbers(numFirst, numSecond);
+    Console.WriteLine($"Сумма натуральных элементов равна: {SumNumbers(numFirst, numSecond)}");
+}
+
+
+int ReadInt(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+int SumNumbers(int numFirst, int numSecond)
+{   
+    if (numFirst == 0) 
+    {
+        return (numSecond * (numSecond + 1)) / 2;
+    }
+    else if (numSecond == 0) 
+    {
+        return (numFirst * (numFirst + 1)) / 2;
+    }
+    else if (numFirst == numSecond) 
+    {
+        return numFirst;
+    }
+    else if (numFirst < numSecond) 
+    {
+        return numSecond + SumNumbers(numFirst, numSecond - 1);
+    }
+    else return numSecond + SumNumbers(numSecond + 1, numFirst);
+   
+}
+
+
+Main();
+
+
---
+++ Exercise002_AckermanFunction/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+
+void Main()
+{
+    int numM = ReadInt ("Введите первое число: ");
+    int numN = ReadInt ("Введите второе число: ");
+    AckermannFunction(numM, numN);
+    Console.WriteLine($"Ackerman function ({numM}, {numN}) = {AckermannFunction(numM, numN)}");
+}
+
+
+int ReadInt(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+int AckermannFunction(int numM, int numN)
+{
+    if (numM == 0)
+    {
+        return numN + 1;
+    }
+    if (numM > 0 && numN == 0) 
+    {
+        return AckermannFunction(numM - 1, 1);
+    }
+    if (numM > 0 && numN > 0) 
+    {
+        return AckermannFunction(numM - 1, AckermannFunction(numM, numN - 1));
+    }
+return AckermannFunction(numM, numN);
+}
+
+
+Main();
